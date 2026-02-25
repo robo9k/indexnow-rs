e2990a6d719db0fb7d30fa79da54bc3dbb58e98e
robot9001 <robo9k@symlink.io>
2026-02-25 18:34:24 +0100

    Parse IndexNow API and return errors
    Signed-off-by: robot9001 <robo9k@symlink.io>

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,8 @@
 pub mod reqwest;
 
-use crate::{ContentUrl, EndpointUrl, KeyfileConfig};
+use crate::{
+    ContentUrl, EndpointUrl, IndexnowError, KeyfileConfig, SubmissionError, SubmissionSuccess,
+};
 use std::error::Error;
 use std::fmt::Debug;
 use tower::ServiceExt as _;
@@ -14,8 +16,11 @@ pub struct Client<S> {
 
 impl<S, B> Client<S>
 where
-    S: tower::Service<http::Request<crate::Body>, Response = http::Response<B>, Error: Error>
-        + Clone,
+    S: tower::Service<
+            http::Request<crate::Body>,
+            Response = http::Response<B>,
+            Error: Error + 'static,
+        > + Clone,
     B: http_body::Body,
 {
     pub fn new(endpoint: EndpointUrl, keyfile: KeyfileConfig, service: S) -> Self {
@@ -26,51 +31,57 @@ where
         }
     }
 
-    pub async fn submit_one(&self, url: &ContentUrl) -> Result<(), ClientError> {
+    pub async fn submit_one(&self, url: &ContentUrl) -> Result<SubmissionSuccess, ClientError> {
         let request = crate::submit_one_request(
             self.endpoint.clone(),
             &self.keyfile.key,
             &self.keyfile.location,
             url,
         )
-        .map_err(|_e| ClientError(()))?;
+        .map_err(ClientError::Request)?;
 
-        let _response = self
+        let response = self
             .service
             .clone()
             .ready()
             .await
-            .map_err(|_e| ClientError(()))?
+            .map_err(|e| ClientError::Tower(Box::new(e)))?
             .call(request)
             .await
-            .map_err(|_e| ClientError(()))?;
+            .map_err(|e| ClientError::Tower(Box::new(e)))?;
 
-        todo!()
+        crate::parse_response(&response).map_err(ClientError::IndexNow)
     }
 
-    pub async fn submit_set(&self, urls: &[ContentUrl]) -> Result<(), ClientError> {
+    pub async fn submit_set(&self, urls: &[ContentUrl]) -> Result<SubmissionSuccess, ClientError> {
         let request = crate::submit_set_request(
             self.endpoint.clone(),
             &self.keyfile.key,
             &self.keyfile.location,
             urls,
         )
-        .map_err(|_e| ClientError(()))?;
+        .map_err(ClientError::Request)?;
 
-        let _response = self
+        let response = self
             .service
             .clone()
             .ready()
             .await
-            .map_err(|_e| ClientError(()))?
+            .map_err(|e| ClientError::Tower(Box::new(e)))?
             .call(request)
             .await
-            .map_err(|_e| ClientError(()))?;
+            .map_err(|e| ClientError::Tower(Box::new(e)))?;
 
-        todo!()
+        crate::parse_response(&response).map_err(ClientError::IndexNow)
     }
 }
 
 #[derive(Debug, thiserror::Error)]
-#[error("Client error")]
-pub struct ClientError(());
+pub enum ClientError {
+    #[error("request")]
+    Request(#[source] IndexnowError),
+    #[error("tower-http client")]
+    Tower(#[source] Box<dyn Error>),
+    #[error("IndexNow API")]
+    IndexNow(#[source] SubmissionError),
+}
